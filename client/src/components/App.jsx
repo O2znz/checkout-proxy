ff0b9cfd5971ce2536ce67fa813740bba88ab51b
Avery <averylisneil@gmail.com>
2019-10-21 19:06:31 -0700

    Repo starter blank and ready.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,21 +8,7 @@ class App extends Component {
     this.state = {
       listingInfo: []
     };
-    this.initialize = this.initialize.bind(this)
   }
-
-  initialize() {
-    axios.get('/listing')
-      .then((response) => {
-        console.log(response.data)
-        this.setState({listingInfo: response.data})
-      });
-  }
-
-  componentDidMount() {
-    this.initialize()
-  }
-
 
   render() {
     return (
--- server/index.js
+++ server/index.js
@@ -1,30 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
-const getFns = require('./controllers/getRequests.js');
+const PORT = 3000;
 const faker = require('faker');
 const bodyparser = require('body-parser');
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyparser.json());
-
-app.get('/listing', (req, res) => {
-    var listingId = faker.random.number({min: 3, max:5})
-    getFns.getInitialData(listingId, (err, data) => {
-        if (err) {
-          res.status(404).send(err)
-        } else {
-          res.status(200).send(data)
-        }
-    })
-})
-
-// app.post('/reservation', (req, res) =>{
-//     var req = req.body;
-
-// })
 
 
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
